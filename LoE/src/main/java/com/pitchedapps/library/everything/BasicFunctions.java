47e448387b007e1f4a5e2906c16ab6e945f0d364
Allan W <asdfasdfvful@gmail.com>
2016-01-07 13:20:10 -0500

    0.2

--- LoE/src/main/java/com/pitchedapps/library/everything/BasicFunctions.java
+++ LoE/src/main/java/com/pitchedapps/library/everything/BasicFunctions.java
@@ -17,11 +17,11 @@ public class BasicFunctions {
     public BasicFunctions(Context c) {
         this.c = c;
     }
-    public void onContact(String email, String subject, String title) {
+    public void contact() {
         StringBuilder emailBuilder = new StringBuilder();
 
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + email));
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + c.getResources().getString(R.string.email_id)));
+        intent.putExtra(Intent.EXTRA_SUBJECT, c.getResources().getString(R.string.email_subject));
 
         emailBuilder.append("Write here");
         emailBuilder.append("\n \nOS Version: ").append(System.getProperty("os.version")).append("(").append(Build.VERSION.INCREMENTAL).append(")");
@@ -40,12 +40,30 @@ public class BasicFunctions {
         emailBuilder.append("\nApp Version Code: ").append(appInfo.versionCode);
 
         intent.putExtra(Intent.EXTRA_TEXT, emailBuilder.toString());
-        c.startActivity(Intent.createChooser(intent, title));
+        c.startActivity(Intent.createChooser(intent, (c.getResources().getString(R.string.send_title))));
     }
 
-    public void onRate() {
-        Intent rate = new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URL + c.getApplicationContext().getPackageName()));
+    public void rate() { //defaults to actual package
+        openPlay(c.getApplicationContext().getPackageName());
+    }
+    public void openPlay (String package_name) { //in case variant is not on the play store
+        Intent rate = new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URL + package_name));
         c.startActivity(rate);
+    }
+
+    public void share() {
+        share(c.getApplicationContext().getPackageName());
+    }
+    public void share(String package_name) {
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody =
+                c.getResources().getString(R.string.share_one) +
+                        c.getResources().getString(R.string.dev) +
+                        c.getResources().getString(R.string.share_two) +
+                        MARKET_URL + package_name;
+        sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+        c.startActivity(Intent.createChooser(sharingIntent, (c.getResources().getString(R.string.share_title))));
     }
 
 
---
+++ sample/src/main/java/com/pitchedapps/sample/everything/MainActivity.java
@@ -0,0 +1,47 @@
+package com.pitchedapps.sample.everything;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.pitchedapps.library.everything.R;
+
+/**
+ * Created by 7681 on 2016-01-07.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.share:
+                break;
+
+        }
+        return true;
+    }
+
+}
